9a97cd5018ddc4a4af076bdaca2e5ac485b9fcd2
Ryan Dooley <Rdoolz51@aol.com>
2022-08-06 01:20:02 -0400

    completed post and get routes for notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,28 +2,35 @@ const express = require('express');
 const router = express.Router();
 const { writeFile, readFile } = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require("uuid");
 
 
 //Gets all notes
 router.get('/notes', (req, res) => {
     readFile(path.join(__dirname, '../../db/db.json'), (err, data) => {
         if (err) throw err;
-        if (!data) {
-            res.json([]);
+        // check to see if the database is empty
+        if (!data[0]) {
+            res.json('[]');
+        } else {
+
+            res.json(JSON.parse(data));
         }
-        res.json(JSON.parse(data));
     });
 });
 
 //Post note
 router.post('/notes', (req, res) => {
     readFile(path.join(__dirname, '../../db/db.json'), (err, data) => {
-        let allnotes = [];
+        let allNotes = [];
         if (err) throw err;
-        if (data) {
-            let allNotes = JSON.parse(data);
-            let newNote = req.body;
+        if (data[0]) {
+            allNotes = JSON.parse(data);
         }
+        let newNote = {
+            ...req.body,
+            id: uuidv4()
+        };
         allNotes.push(newNote);
 
         writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(allNotes), (err) => {
@@ -33,6 +40,8 @@ router.post('/notes', (req, res) => {
     });
 
 });
+
+
 
 
 
